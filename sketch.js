9749705425f466ab01336f9a07b2fc843418693e
claireeehsu <115965694+claireeehsu@users.noreply.github.com>
2022-10-24 11:16:12 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,15 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
- 
-  rectMode(CENTER)//設定方框的座標點為中心點
-  noFill()//不充滿顏色
-  //stroke(255)
- 
-  background(0)
-  for(var j=0;j<int(height/(49));j++)
-		{
-  for(var i=0;i<int(width/(48));i++)
-      {
-	stroke(	"#FFAAD5")
-	ellipse(25+(i*50),25+(j*50),50+(mouseX+mouseY)/100)
-	stroke("#46A3FF")
-  rect(25+(i*50),25+(j*50),50+(mouseX+mouseY)/10)
-	stroke(	"#B15BFF")
-  ellipse(50+(i*50),50+(j*50),20+(mouseX+mouseY)/50)
-	    }
-	  }		
-	}
+  background(220);
+  textSize(20)
+  text("x:"+mouseX+"Y:"+mouseY,50,50)
+  ellipse(width/2,height/2,400)//臉
+  ellipse(width/2,height/2,200,100)//鼻子橢圓
+  ellipse(width/2-40,height/2,30)//左鼻孔
+  ellipse(width/2+40,height/2,30)//右鼻孔
+  ellipse(width/2-100,height/2-80,30)
+}
+
